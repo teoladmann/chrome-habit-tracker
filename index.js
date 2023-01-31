e8fab656136ab260fddb3ade338b21e0a242f197
Timoteo Ladmann <teoladmann@gmail.com>
2023-01-31 19:46:19 -0300

    Some issues fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //Declare global variables
-let habitStorage = {'habits': []}; //For updating sync storage
+let habitStorage = { habits: [] }; //For updating sync storage
 let actualDays; //Days in selected month for rendering
 let monthSelected = new Date().getMonth(); //Actual month selected 0-indexed
 
@@ -10,32 +10,40 @@ document.addEventListener('DOMContentLoaded', () => {
     //Check sync storage
     if (habits.hasOwnProperty('habits')) Object.assign(habitStorage, habits);
     //Chrome doesn't allow this to be put directly on index.html
-    document.getElementById('add-habit-button').addEventListener('click', showInput);
+    document
+      .getElementById('add-habit-button')
+      .addEventListener('click', showInput);
     //Render grid
     setGrid(new Date().getMonth());
   });
-
 });
 
-const getDaysInMonth = (month) => { //Month passed as 1-indexed
+const getDaysInMonth = (month) => {
+  //Month passed as 1-indexed
   return new Date(new Date().getFullYear(), month + 1, 0).getDate();
-}
+};
 
-const createNode = (type, classGiven) => { //Shortcut for creating nodes
+const createNode = (type, classGiven) => {
+  //Shortcut for creating nodes
   const node = document.createElement(type);
   node.className = classGiven;
   return node;
-}
+};
 
 const setGrid = (month) => {
-
-  const daysInMonth = actualDays = getDaysInMonth(month);
+  const daysInMonth = (actualDays = getDaysInMonth(month));
 
   const gridContainerNode = createNode('div', 'grid-main-container');
   gridContainerNode.setAttribute('id', 'grid-main-container');
-  gridContainerNode.style['grid-template-rows'] = `repeat(${2 + habitStorage.habits.length}, 1fr)`;
-  gridContainerNode.style['grid-template-columns'] = `225px repeat(${daysInMonth}, 1fr)`;
-  document.getElementById('main-title').insertAdjacentElement('afterend', gridContainerNode);
+  gridContainerNode.style['grid-template-rows'] = `repeat(${
+    2 + habitStorage.habits.length
+  }, 1fr)`;
+  gridContainerNode.style[
+    'grid-template-columns'
+  ] = `225px repeat(${daysInMonth}, 1fr)`;
+  document
+    .getElementById('main-title')
+    .insertAdjacentElement('afterend', gridContainerNode);
 
   const monthTitleNode = createNode('div', 'month-title title');
   monthTitleNode.appendChild(document.createTextNode('MONTH'));
@@ -55,11 +63,23 @@ const setGrid = (month) => {
   renderDays(month);
 
   renderHabits();
-
-}
+};
 
 const renderMonths = (month) => {
-  const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+  const months = [
+    'JAN',
+    'FEB',
+    'MAR',
+    'APR',
+    'MAY',
+    'JUN',
+    'JUL',
+    'AUG',
+    'SEP',
+    'OCT',
+    'NOV',
+    'DEC',
+  ];
   for (let i = 0; i < months.length; i++) {
     const monthNode = createNode('span', 'month title');
     monthNode.setAttribute('id', `m${i}`);
@@ -76,7 +96,7 @@ const renderMonths = (month) => {
       document.getElementById('month-container').appendChild(monthNode);
     }
   }
-}
+};
 
 const renderDays = (month) => {
   const daysInMonth = getDaysInMonth(month);
@@ -84,8 +104,8 @@ const renderDays = (month) => {
     const dayNode = createNode('span', 'day');
     if (i === 0) dayNode.classList.add('first-one');
     if (i === daysInMonth - 1) dayNode.classList.add('last-one');
-    if (new Date().getMonth() === month && (i + 1) === new Date().getDate()) {
-      const actualDayNode = createNode('p', 'actual-day')
+    if (new Date().getMonth() === month && i + 1 === new Date().getDate()) {
+      const actualDayNode = createNode('p', 'actual-day');
       const textNode = document.createTextNode(String(i + 1));
       actualDayNode.appendChild(textNode);
       dayNode.appendChild(actualDayNode);
@@ -96,35 +116,43 @@ const renderDays = (month) => {
       document.getElementById('grid-main-container').appendChild(dayNode);
     }
   }
-}
+};
 
 const selectMonth = (e) => {
   const month = Number(e.target.id.slice(1));
   monthSelected = month;
   document.getElementById('grid-main-container').remove();
   setGrid(month);
-}
+};
 
 const renderHabits = () => {
-  document.getElementById('grid-main-container').style['grid-template-rows'] = `repeat(${2 + habitStorage.habits.length}, 1fr)`;
+  document.getElementById('grid-main-container').style[
+    'grid-template-rows'
+  ] = `repeat(${2 + habitStorage.habits.length}, 1fr)`;
   for (let i = 0; i < habitStorage.habits.length; i++) {
-    habitStorage.habits[i].id = i + 1;
     const habitContainerNode = createNode('div', 'habit-container');
-    document.getElementById('grid-main-container').appendChild(habitContainerNode);
-    habitContainerNode.setAttribute('id', `habit-container-${i + 1}`);
-    habitContainerNode.addEventListener('mouseover', showRemove);
-    habitContainerNode.addEventListener('mouseout', hideRemove);
+    document
+      .getElementById('grid-main-container')
+      .appendChild(habitContainerNode);
+    habitContainerNode.setAttribute(
+      'id',
+      `habit-container-${habitStorage.habits[i].id}`
+    );
     const removeNode = createNode('span', 'remove-habit');
-    removeNode.setAttribute('id', `remove-${i + 1}`);
+    removeNode.setAttribute('id', habitStorage.habits[i].id);
     removeNode.addEventListener('click', removeHabit);
     const removeTextNode = document.createTextNode('X');
     removeNode.appendChild(removeTextNode);
-    document.getElementById(`habit-container-${i + 1}`).appendChild(removeNode);
+    document
+      .getElementById(`habit-container-${habitStorage.habits[i].id}`)
+      .appendChild(removeNode);
     const habitNode = createNode('span', 'habit');
     const textNode = document.createTextNode(habitStorage.habits[i].name);
     habitNode.appendChild(textNode);
-    habitNode.setAttribute('id', `habit-${i + 1}`);
-    document.getElementById(`habit-container-${i + 1}`).appendChild(habitNode);
+    habitNode.setAttribute('id', `habit-${habitStorage.habits[i].id}`);
+    document
+      .getElementById(`habit-container-${habitStorage.habits[i].id}`)
+      .appendChild(habitNode);
     for (let j = 0; j < actualDays; j++) {
       const checkNode = createNode('input', 'check');
       checkNode.setAttribute('id', `checkbox-${i + '-' + j}`);
@@ -136,58 +164,62 @@ const renderHabits = () => {
       checkContainerNode.appendChild(checkNode);
       checkContainerNode.appendChild(labelCheckNode);
       if (j === actualDays - 1) checkContainerNode.classList.add('last-one');
-      document.getElementById('grid-main-container').appendChild(checkContainerNode);
-      // input value logic: HABIT ID 2 DIGITS | YEAR 4 DIGITS | MONTH 2 DIGITS | DAY 2 DIGITS
-      const dateValue = `${String(habitStorage.habits[i].id)}${new Date().getFullYear()}${monthSelected < 10 ? '0' + String(monthSelected + 1) : monthSelected + 1}${j + 1 < 10 ? '0' + String(j + 1) : String(j + 1)}`;
-      const dateValueIdLessThan10 = `0${dateValue}`;
-      checkNode.setAttribute('value', `${habitStorage.habits[i].id < 10 ? dateValueIdLessThan10 : dateValue}`);
-      if (habitStorage.habits[i].dates.hasOwnProperty(checkNode.value) && habitStorage.habits[i].dates[checkNode.value] === 1) {
+      document
+        .getElementById('grid-main-container')
+        .appendChild(checkContainerNode);
+      // input value logic: YEAR 4 DIGITS | MONTH 2 DIGITS | DAY 2 DIGITS
+      const dateValue = `${new Date().getFullYear()}${
+        monthSelected < 10 ? '0' + String(monthSelected + 1) : monthSelected + 1
+      }${j + 1 < 10 ? '0' + String(j + 1) : String(j + 1)}`;
+      checkNode.setAttribute('value', `${dateValue}`);
+      checkNode.setAttribute('name', habitStorage.habits[i].id);
+      if (habitStorage.habits[i].dates.includes(checkNode.value)) {
         checkNode.setAttribute('checked', true);
       }
       checkNode.addEventListener('click', toggleCheck);
-      document.getElementById('grid-main-container').appendChild(checkContainerNode);
+      document
+        .getElementById('grid-main-container')
+        .appendChild(checkContainerNode);
     }
   }
-}
+};
 
 const toggleCheck = (e) => {
-  const habitClicked = habitStorage.habits[Number(e.target.value.slice(0, 2)) - 1]; // habits arr is 0 - indexed and habits id are 1 - indexed
-  if (habitClicked.dates.hasOwnProperty(e.target.value)) {
-    if (habitClicked.dates[e.target.value] === 0) {
-      habitClicked.dates[e.target.value] = 1;
-    } else {
-      habitClicked.dates[e.target.value] = 0;
-    }
+  const habitClicked = habitStorage.habits.find((h) => h.id === e.target.name);
+  if (habitClicked.dates.includes(e.target.value)) {
+    habitClicked.dates = habitClicked.dates.filter((d) => d !== e.target.value);
   } else {
-    habitStorage.habits[Number(e.target.value.slice(0, 2)) - 1].dates[e.target.value] = 1;
+    habitClicked.dates.push(e.target.value);
   }
-  chrome.storage.sync.set(habitStorage, () => console.log('Check toggled'));
-}
+  chrome.storage.sync.set(habitStorage);
+};
 
 const showInput = () => {
   document.getElementById('add-habit-container').style['display'] = 'none';
   document.getElementById('habit-input').addEventListener('blur', hideInput);
   document.getElementById('habit-input').addEventListener('keydown', keyInput);
   document.getElementById('habit-input-container').style['display'] = 'flex';
-  document.getElementById('habit-input-container').style['align-items'] = 'center';
+  document.getElementById('habit-input-container').style['align-items'] =
+    'center';
   document.getElementById('habit-input').blur();
   document.getElementById('habit-input').focus();
-}
+};
 
 const addHabit = (habit) => {
-  const newHabitId = habitStorage.habits.length + 1;
-  habitStorage.habits.push({'id': newHabitId, 'name': habit, 'dates': {}});
-  chrome.storage.sync.set(habitStorage, () => console.log('Added habit'));
+  if (habitStorage.habits.some((h) => h.name === habit)) return;
+  const newHabitId = `${Date.now()}`;
+  habitStorage.habits.push({ id: newHabitId, name: habit, dates: [] });
+  chrome.storage.sync.set(habitStorage);
   document.getElementById('grid-main-container').remove();
   document.getElementById('habit-input').value = '';
   hideInput();
   setGrid(monthSelected);
-}
+};
 
 const hideInput = () => {
   document.getElementById('habit-input-container').style['display'] = 'none';
   document.getElementById('add-habit-container').style['display'] = 'flex';
-}
+};
 
 const keyInput = (e) => {
   let val = document.getElementById('habit-input').value;
@@ -201,22 +233,11 @@ const keyInput = (e) => {
   } else {
     val += String.fromCharCode(e.keyCode);
   }
-}
+};
 
 const removeHabit = (e) => {
-  habitStorage.habits.splice(Number(e.target.id.slice(7)) - 1, 1);
-  chrome.storage.sync.set(habitStorage, () => console.log('Removed habit'));
+  habitStorage.habits = habitStorage.habits.filter((h) => h.id !== e.target.id);
+  chrome.storage.sync.set(habitStorage);
   document.getElementById('grid-main-container').remove();
   setGrid(monthSelected);
-}
-
-const showRemove = (e) => {
-  const idNumber = e.target.id.match(/[0-9]+/gm);
-  document.getElementById('remove-' + idNumber).style['opacity'] = 0.5;
-}
-
-const hideRemove = (e) => {
-  const idNumber = e.target.id.match(/[0-9]+/gm);
-  document.getElementById('remove-' + idNumber).style['opacity'] = 0;
-}
-
+};
